ea97e74db9a4daa78d31c83a857577855a8462f5
dtaylor <dtaylor@TrainingVM>
2021-12-17 19:31:32 -0600

    Refactor tests to use abstract type

--- basics/src/test/java/basics/CarTests.java
+++ basics/src/test/java/basics/CarTests.java
@@ -20,12 +20,22 @@ public class CarTests {
 	@Test
 	public void canDriveBicycle() {
 		Bicycle bike = new Bicycle();
+		driveVehicle(bike);
+	}
+	
+	@Test
+	public void canDriveCar() {
+		Car car = new Car("Corvette");
+		driveVehicle(car);
+	}
+	
+	private void driveVehicle(Vehicle vehicle) {
 		int distance = 1;
 		
-		int odometerBefore = bike.getOdometer();
-		bike.drive(distance);
+		int odometerBefore = vehicle.getOdometer();
+		vehicle.drive(distance);
 		int expectedOdometer = odometerBefore + distance;
-		int actualOdometer = bike.getOdometer();
+		int actualOdometer = vehicle.getOdometer();
 		
 		assertEquals("the odometer should be incremented by the distance.", expectedOdometer, actualOdometer);
 	}
